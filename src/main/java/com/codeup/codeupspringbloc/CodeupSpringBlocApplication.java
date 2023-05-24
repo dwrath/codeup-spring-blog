083f041c039b8e95a089251dfb3264e94ee1d3ff
Damian Wrather <damian.wrather@gmail.com>
2023-05-24 09:32:13 -0500

    Initial commit

---
+++ src/main/java/com/codeup/codeupspringbloc/CodeupSpringBlocApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.codeupspringbloc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeupSpringBlocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeupSpringBlocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeup/codeupspringbloc/CodeupSpringBlocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.codeupspringbloc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeupSpringBlocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
