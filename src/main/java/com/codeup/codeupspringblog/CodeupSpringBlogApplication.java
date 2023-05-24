3a995b3791f383f9202dc0e81c609423b3e8a607
Damian Wrather <damian.wrather@gmail.com>
2023-05-24 09:47:39 -0500

    adding .gitignore

--- src/main/java/com/codeup/codeupspringblog/CodeupSpringBlogApplication.java
+++ src/main/java/com/codeup/codeupspringblog/CodeupSpringBlogApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CodeupSpringBlocApplication {
+public class CodeupSpringBlogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CodeupSpringBlocApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CodeupSpringBlogApplication.class, args);
+    }
 
 }
--- src/test/java/com/codeup/codeupspringblog/CodeupSpringBlocApplicationTests.java
+++ src/test/java/com/codeup/codeupspringblog/CodeupSpringBlocApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CodeupSpringBlocApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
